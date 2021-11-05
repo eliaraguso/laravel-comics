8644d20fa319b75dbca404b1e8a45aeae7bcb9ef
eliaraguso <elia.raguso@gmail.com>
2021-11-05 15:45:59 +0100

    Creata struttura base

--- public/js/app.js
+++ public/js/app.js
@@ -19432,8 +19432,8 @@ window.axios.defaults.headers.common['X-Requested-With'] = 'XMLHttpRequest';
 /*! no static exports found */
 /***/ (function(module, exports, __webpack_require__) {
 
-__webpack_require__(/*! E:\BOOLEAN\ESERCIZI\53 - Laravel pasta molisana\resources\js\app.js */"./resources/js/app.js");
-module.exports = __webpack_require__(/*! E:\BOOLEAN\ESERCIZI\53 - Laravel pasta molisana\resources\sass\app.scss */"./resources/sass/app.scss");
+__webpack_require__(/*! E:\BOOLEAN\ESERCIZI\53 - Laravel DC comics\resources\js\app.js */"./resources/js/app.js");
+module.exports = __webpack_require__(/*! E:\BOOLEAN\ESERCIZI\53 - Laravel DC comics\resources\sass\app.scss */"./resources/sass/app.scss");
 
 
 /***/ })
---
+++ resources/views/index.blade.php
@@ -0,0 +1,7 @@
+@extends('layout.base')
+
+@section('pageContent')
+<section>
+    <h1>MAIN</h1>
+</section>
+@endsection
