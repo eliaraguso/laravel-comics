dd819eae488e602c9df86352fb7ecb7be5f7bef1
eliaraguso <elia.raguso@gmail.com>
2021-11-06 00:50:27 +0100

    aggiornato header

--- resources/views/partials/header.blade.php
+++ resources/views/partials/header.blade.php
@@ -8,7 +8,22 @@
         </div>
         <div class="header-bottom">
             <div class="container">
-                
+                <img src="../images/dc-logo.png" alt="">
+                <nav>
+                    <ul>
+                        <li><a href="#">CHARACTERS</a></li>
+                        <li><a href="#">COMICS</a></li>
+                        <li><a href="#">MOVIES</a></li>
+                        <li><a href="#">TV</a></li>
+                        <li><a href="#">GAMES</a></li>
+                        <li><a href="#">COLLECTIBLES</a></li>
+                        <li><a href="#">VIDEOS</a></li>
+                        <li><a href="#">FANS</a></li>
+                        <li><a href="#">NEWS</a></li>
+                        <li><a href="#">SHOP</a></li>
+                    </ul>
+                </nav>
+                <input type="text" name="" id="" placeholder="Search">
             </div>
         </div>
     
