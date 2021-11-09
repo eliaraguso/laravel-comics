ccf7528322e0ad566b2a9e7838c852eaa92f4eab
eliaraguso <elia.raguso@gmail.com>
2021-11-09 15:01:41 +0100

    aggiunta hero section + comics section(in manierda dinamica)

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -1,8 +1,23 @@
 @extends('layout.base')
 
 @section('pageContent')
-<section>
-    <h1>main</h1>
+
+    <section class="hero"></section>
+    <div class="current-series">CURRENT SERIES</div>
+    <div class="container">
+        <section class="comics">
+            @foreach ($fumetti as $fumetto)
+                <div class="card">
+                    <div class="card-image">
+                        <img src="{{ $fumetto['thumb'] }}" alt="{{ $fumetto['title'] }} image">
+                    </div>
+                    <div class="card-title">
+                        {{ $fumetto['title'] }}
+                    </div>
+                </div>
+            @endforeach
+        </section>
+    </div>
     {{-- @dd($fumetti); --}}
-</section>
+
 @endsection
